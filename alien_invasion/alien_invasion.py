4efedcd6da289d0f694b47d51d9bd6c0a57b1cbd
Makar Kalancha <makarkalancha>
2023-12-21 17:33:33 -0500

    p290

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -34,13 +34,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            
-            # Get rid of bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            
+            self._update_bullets()
             self._update_screen()
             
     def _check_events(self):
@@ -88,6 +82,16 @@ class AlienInvasion:
         
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+        
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+            
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
             
 if __name__ == '__main__':
     # Make a game instance, and run the game
