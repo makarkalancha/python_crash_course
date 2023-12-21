32537ae46f95e5d9872443c775059821ad41d3b6
Makar Kalancha <makarkalancha>
2023-12-21 14:08:49 -0500

    p279

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         
         # Load the ship image and get its rect.
@@ -14,6 +15,9 @@ class Ship:
         
         # Start each new ship at the botton center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
         
         # Movement flag
         self.moving_right = False
@@ -24,10 +28,14 @@ class Ship:
         self.screen.blit(self.image, self.rect)
         
     def update(self):
-        """Update the ship's position based on the movement flag."""
+        """Update the ship's position based on the movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         elif self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+            
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
         
