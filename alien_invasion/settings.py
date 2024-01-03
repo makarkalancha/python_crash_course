b8f4cae779fc302b22225975d4231d5ae7131573
Makar Kalancha <makarkalancha>
2024-01-02 23:14:03 -0500

    p311

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -13,6 +13,7 @@ class Settings:
         
         # Bullet settings
         self.bullet_speed = 1.0
+        self.ship_limit = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
