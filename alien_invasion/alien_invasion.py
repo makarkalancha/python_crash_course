90f1ea21242a2af3cd3a70f841070cbfd2c08881
Makar Kalancha <makarkalancha>
2024-02-09 12:25:23 -0500

    p321

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -65,6 +65,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
                 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -217,6 +220,11 @@ class AlienInvasion:
                 # Treat this the same as if the ship got hit.
                 self._ship_hit()
                 break
+            
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
