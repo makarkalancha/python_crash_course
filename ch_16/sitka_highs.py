8e657d2f8ca3bcbf824d649a5d9c88be8425362e
Makar Kalancha <makarkalancha>
2024-02-10 00:22:39 -0500

    moving files

--- ch_16/sitka_highs.py
+++ ch_16/sitka_highs.py
@@ -1,6 +1,6 @@
 import csv
 
-filename = 'sitka_weather_07-2018_simple.csv'
+filename = 'Crash_course/ch_16/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
