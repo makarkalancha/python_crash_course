5bdf52ad2462c4503af0e978ce3253ee04c13cc0
Makar Kalancha <makarkalancha>
2024-02-09 23:12:55 -0500

    p348

--- graph/mpl_squares.py
+++ graph/mpl_squares.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+plt.style.use('classic')
+# plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth = 3)
 
