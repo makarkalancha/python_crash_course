e5a2b4a4a40f60e452394a4456b222fad4828adf
Makar Kalancha <makarkalancha>
2024-02-09 23:50:43 -0500

    p359

--- graph/rw_visual.py
+++ graph/rw_visual.py
@@ -17,7 +17,12 @@ while True:
     
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+               s=100)
+    
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     
     plt.show()
     
